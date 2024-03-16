69237d9765136a2b3f5777b830c1ce632de88cae
Oscarharis <oscarharisn1928@gmail.com>
2024-03-16 17:04:23 +0700

    latihan string

---
+++ latihan3/biodata.py
@@ -0,0 +1,18 @@
+# membuat variabel biodata
+nama = " oscar haris ngaratu"
+kelas = "TI23B"
+umur = "29"
+semester = "2"
+jurusan = "teknik informatika"
+quote = "belajarlah dengan giat, agar masa depanmu lebih cerah"
+married = False
+adalahPerempuan = False
+hobi = ["main bola", "nonton"]
+
+print("nama:", nama)
+print("kelas:", kelas)
+print("umur:", umur)
+print("semester:", semester)
+print("jurusan:",jurusan)
+print("quote:", quote)
+print(f"hobi: {hobi[0]} dan {hobi[1]}.")
