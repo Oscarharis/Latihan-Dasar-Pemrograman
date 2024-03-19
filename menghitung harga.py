892ff6121ab194fd999a73d8901f5e6702a72863
Oscarharis <147423427+Oscarharis@users.noreply.github.com>
2024-03-19 19:20:50 +0700

    menghitung harga.py

--- menghitung harga.py
+++ menghitung harga.py
@@ -1,11 +1,29 @@
-#
-hargaSupplier = int(input("silahkan Masukan Harga Dari Suplier : "))
-persen = int(input("silahkan Masukan persentase : "))
-hargaPersen = hargaSupplier *(persen /100)
-hargaJual =  hargaSupplier + hargaPersen
+# Daftar varian kosmetik
+varian = ["Lipstik", "Bedak", "Eyeshadow", "Maskara"]
 
-print("Harga Dari Supplier :",hargaSupplier)
-print("Persentase keuntungan :",persen)
-print("Keuntungan Dari Persen : ",hargaPersen)
-print("Harga Jual : ",hargaJual)
-print("\n")
+# Daftar harga dari supplier
+harga_supplier = [100000, 50000, 75000, 60000]
+
+# Persentase keuntungan
+persentase_keuntungan = 10
+
+# Hitung harga jual
+harga_jual = []
+for i in range(len(varian)):
+  harga_jual.append(harga_supplier[i] + (harga_supplier[i] * persentase_keuntungan / 100))
+
+# Tampilkan tabel daftar kosmetik
+print("=" * 30)
+print("Daftar Kosmetik")
+print("=" * 30)
+print("| Varian | Harga Supplier | Harga Jual |")
+print("|---|---|---|")
+for i in range(len(varian)):
+  print("| {} | Rp {} | Rp {} |".format(varian[i], harga_supplier[i], harga_jual[i]))
+print("=" * 30)
+
+# Tampilkan total keuntungan
+total_keuntungan = 0
+for i in range(len(varian)):
+  total_keuntungan += harga_jual[i] - harga_supplier[i]
+print("Total Keuntungan: Rp {}".format(total_keuntungan))
