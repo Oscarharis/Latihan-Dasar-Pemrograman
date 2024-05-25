68ac9690cc2d3ffe05692eecf98cd439045cee31
Oscarharis <147423427+Oscarharis@users.noreply.github.com>
2024-05-25 21:37:47 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+import Modul.modulBiodata as Biodata
+
+biodataOscar = Biodata.getBiodata("Oscar","TI23B","SDN 5 PASIR HALANG", "SMP 1 SUKARAJA")
+print(biodataOscar)
+biodataOscar = Biodata.getBiodata("oscar haris.n","TI23B","SDN 5  pasir halang", "SMP 1 SUKARAJA")
+print(biodataOscar)
